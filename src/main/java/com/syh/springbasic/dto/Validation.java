48bf499682a175321c7d623be5e8ad8583f1edac
y0001ha <yyy96110@naver.com>
2024-08-22 17:23:58 +0900

    20240822 17:23

--- src/main/java/com/syh/springbasic/dto/Validation.java
+++ src/main/java/com/syh/springbasic/dto/Validation.java
@@ -1,10 +1,15 @@
 package com.syh.springbasic.dto;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -44,5 +49,20 @@ public class Validation {
     @NotNull
     private String length;
     
+    // @Min(), @Max() : 정수의 최소 최대 값을 지정
+    @Min(10)
+    @Max(20)
+    private Integer minMax;
 
+    // @Range(min=?, max=?)
+    @Range(min=5, max=25)
+    private Integer range;
+
+    // @Size(min=?, max=?): 컬렉션의 길이 최소 최대값을 지정
+    @Size(min=2, max=4)
+    private Integer[] size; 
+
+    // @Pattern(regexp=정규식): 정규식을 이용하여 문자열의 패턴을 검사
+    @Pattern(regexp="^[a-zA-Z]*$")
+    private String pattern;
 }
