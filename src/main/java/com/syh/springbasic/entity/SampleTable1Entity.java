f8c326d350cd237e66d253421d7db36d4c6ab2a0
y0001ha <yyy96110@naver.com>
2024-08-26 17:30:23 +0900

    20240826 17:30

--- src/main/java/com/syh/springbasic/entity/SampleTable1Entity.java
+++ src/main/java/com/syh/springbasic/entity/SampleTable1Entity.java
@@ -1,6 +1,8 @@
 package com.syh.springbasic.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +17,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-// @Entity:
+// @Entity:n
 // - 해당 클래스를 Entity 클래스로 지정하는 어노테이션
 // - JPA에서 데이터 관리를 위해 사용되는 주된 객체
 // - name 속성으로 Entity 자체의 이름을 지정할 수 있음
@@ -27,6 +29,27 @@ import lombok.Setter;
 @Table(name="sample_table1")
 // Sample_table1_entity
 public class SampleTable1Entity {
+
+    // @Id: 
+    // - Entity 필드 중 PRIMARY KEY로 지정할 필드에 사용하는 어노테이션
+    @Id
+    // @GeneratedValue:
+    // - PRIMARY KEY에서 값 자동 생성 전략을 지정하는 어노테이션
+    // - strategy 속성으로 전략을 지정
+    // - GenerationType.AUTO : JPA가 적절한 생성 전략을 선택
+    // - GenerationTyoe.IDENTITY : auto_increment 전략 선택
+    // - GenerationType.SEQUENCE : 데이터베이스의 sequence 생성 전략 선택
+    // - GenerationType.TABLE : 키 생성 목적의 테이블을 이용하여 생성하는 전략 선택
+    @Column(name="sample_id", nullable=false, unique= true, updatable=false, length=10)
     private String sampleId;
+
+    // @Column:
+    // - 해당 필드를 테이블의 컬럼과 매핑하는 어노테이션
+    // - name: 매핑할 컬럼의 이름
+    // - nullable: null 포함 여부
+    // - unique: unique 제약 여부
+    // - length: 컬럼의 길이
+    // - 만약 테이블의 컬럼명과 클래스의 필드명이 같다면 생략 가능
+    @Column(name="sample_column", nullable=false)
     private Integer sampleColumn;
 }
