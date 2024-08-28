638ba369815be11e4c18c81b2c439b5ff904c21d
y0001ha <yyy96110@naver.com>
2024-08-28 17:19:01 +0900

    20240828 17:50

--- src/main/java/com/syh/springbasic/repository/SampleUserRepository.java
+++ src/main/java/com/syh/springbasic/repository/SampleUserRepository.java
@@ -10,21 +10,23 @@ import org.springframework.stereotype.Repository;
 import com.syh.springbasic.entity.SampleUserEntity;
 
 @Repository
-public interface SampleUserRepository 
-extends JpaRepository<SampleUserEntity, String> {
-    
+public interface SampleUserRepository
+        extends JpaRepository<SampleUserEntity, String> {
+
     // Query Method:
-    // - Repository의 메서드 선언시에 지정된 패턴에 따라 메서드명을 작성하면 JPA가  SQL을 만들어주는 방법
+    // - Repository의 메서드 선언시에 지정된 패턴에 따라 메서드명을 작성하면 JPA가 SQL을 만들어주는 방법
     // - findBy : 필드명을 기준으로 모든 컬럼을 조회할 때 사용, findBy 뒤에 필드명을 붙여서 작성,
-    //            필드명의 첫글자는 대문자이어야함
-    List<SampleUserEntity> findByName(String name); //조회결과 없으면 빈리스트
-    SampleUserEntity findByTelNumber(String telNumber); //조회결과 없으면 null
+    // 필드명의 첫글자는 대문자이어야함
+    List<SampleUserEntity> findByName(String name); // 조회결과 없으면 빈리스트
 
-    // - And/Or : and 연산 혹은 or 연산에 사용됨, 필드와 필드 사이에 사용 
-    //   And/Or의 우선순위 주의
-    List<SampleUserEntity> findByNameAndAddress (String name, String address);
+    SampleUserEntity findByTelNumber(String telNumber); // 조회결과 없으면 null
 
-    // - Like, NotLike, StartingWith, EndingWith, Containing: Like 연산에 대하여 사용, 필드 뒤에 사용
+    // - And/Or : and 연산 혹은 or 연산에 사용됨, 필드와 필드 사이에 사용
+    // And/Or의 우선순위 주의
+    List<SampleUserEntity> findByNameAndAddress(String name, String address);
+
+    // - Like, NotLike, StartingWith, EndingWith, Containing: Like 연산에 대하여 사용, 필드 뒤에
+    // 사용
     List<SampleUserEntity> findByAddressContaining(String address);
 
     // - OrderBy: 정렬을 사용할 필드를 짖어하여 Desc, Asc 지정
@@ -42,11 +44,24 @@ extends JpaRepository<SampleUserEntity, String> {
 
     // JPQL (Java Persistence Query Language)
     // - 표준 SQL과 매우 흡사하지만 Entity 클래스와 Entity 필드로 쿼리를 작성하는 방법
-    @Query(value="SELECT u FROM user u WHERE u.name = ?1 AND u.address = ?2")
+    @Query(value = "SELECT u FROM user u WHERE u.name = ?1 AND u.address = ?2")
     List<SampleUserEntity> getJpql(String name, String address);
 
-    @Query(value="SELECT u FROM user u WHERE u.name = :name AND u.address = :address")
+    @Query(value = "SELECT u FROM user u WHERE u.name = :name AND u.address = :address")
     List<SampleUserEntity> getJpql2(
+            @Param("name") String name,
+            @Param("address") String address);
+    
+            // Native SQL:
+    // - 현재 REBMS의 SQL 문법을 그대로 사용하는 방법
+    // - @Query nativeQuery 속성을 반드시 true로 지정
+    @Query(value=
+    "SELECT * " +
+    "FROM sample_user " +
+    "WHERE name = :name " +
+    "AND address = :address"
+    , nativeQuery = true)
+    List<SampleUserEntity> getNativeSql(
         @Param("name") String name,
         @Param("address") String address
     );
