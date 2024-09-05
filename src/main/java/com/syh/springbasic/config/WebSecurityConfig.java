7f13ef7fb7ece2532b6500eac017d6189dc65051
y0001ha <yyy96110@naver.com>
2024-09-05 15:49:18 +0900

    20240905 15:50

--- src/main/java/com/syh/springbasic/config/WebSecurityConfig.java
+++ src/main/java/com/syh/springbasic/config/WebSecurityConfig.java
@@ -83,7 +83,7 @@ public class WebSecurityConfig {
                 // permitAll(): 모든 클라이언트가 접근할 수 있도록 지정
                 // hasRole(권한): 특정 권한을 가진 클라이언트만 접근할 수 있도록 지정
                 // authenticated(): 인증된 모든 클라이언트가 접근할 수 있도록 지정
-                .requestMatchers("/anyone/**", "/auth/**").permitAll()
+                .requestMatchers("/anyone/**", "/auth/**", "/oauth2/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/sample/jwt/*").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/service").hasRole("ADMIN")
@@ -94,6 +94,13 @@ public class WebSecurityConfig {
                 .anyRequest().authenticated()
             )
             
+            // OAuth2 인증 처리
+            .oauth2Login(oauth2 -> oauth2
+                .redirectionEndpoint(endPoint -> endPoint.baseUri("/oauth2/callback/*"))
+            )
+
+
+
             // 인증 및 인가 과정에서 발생한 예외를 직접 처리
             .exceptionHandling(exceptionHandling -> exceptionHandling
                 .authenticationEntryPoint(new FailedAuthenticationEntryPoint())
